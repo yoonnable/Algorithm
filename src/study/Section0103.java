017eb9b500094ca8cf1ef0faf848084d4ee3f3a1
yoonna <yoonnable@gmail.com>
2023-04-22 22:03:19 +0900

    Section1.3. 문장 속 단어 Section1.4. 단어 뒤집기

---
+++ src/study/Section0103.java
@@ -0,0 +1,43 @@
+package study;
+
+import java.util.Scanner;
+
+public class Section0103 {
+	// split 规侥
+	public String solution1(String str) {
+		String answer = "";
+		String[] pos = str.split(" ");
+		for(String tem : pos) {
+			if(answer.length() < tem.length()) {
+				answer = tem;
+			}
+		}
+		
+		return answer;
+	}
+	
+	//indexOf + substring 规侥
+	public String solution2(String str) {
+		String answer = "";
+		while(str.indexOf(" ") != -1) {
+			if(answer.length() < (str.substring(0, str.indexOf(" "))).length()) answer = str.substring(0, str.indexOf(" "));
+			str = str.substring(str.indexOf(" ") + 1);
+		}
+		if(str.indexOf(" ") == -1) {
+			answer = answer.length() < str.length() ? str : answer;
+		}
+		
+		return answer;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String str = kb.nextLine();
+		Section0103 s3 = new Section0103();
+		System.out.println("split 规侥 : " + s3.solution1(str));
+		System.out.println("indexOf + substring 规侥 : " + s3.solution2(str));
+	}
+
+}
---
+++ src/study/Section0104.java
@@ -0,0 +1,49 @@
+package study;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Section0104 {
+	//StringBuilder
+	public ArrayList<String> solution1(String[] str) {
+		ArrayList<String> answer = new ArrayList<String>();
+		for(String s : str) {
+			answer.add(new StringBuilder(s).reverse().toString());
+		}
+		return answer;
+	}
+	
+	//데칼코마니 스타일로 뒤집기  *Strin.valueOf()
+	public ArrayList<String> solution(String[] str) {
+		ArrayList<String> answer = new ArrayList<String>();
+		for(String s : str) {
+			char[] c = s.toCharArray();
+			int lt = 0, rt = s.length() - 1;
+			while(lt < rt) {
+				char tmp = c[lt];
+				c[lt] = c[rt];
+				c[rt] = tmp;
+				lt++;
+				rt--;
+			}
+			String a = String.valueOf(c);
+			answer.add(a);
+			
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Section0104 s4 = new Section0104();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		String[] str = new String[n];
+		for(int i=0; i < n; i++) {
+			str[i] = kb.next();
+		}
+		for(String s : s4.solution(str)) {
+			System.out.println(s);
+		}
+	}
+
+}
