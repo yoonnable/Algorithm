2c92fc9e1c50cfe2a0a48e890e32470b3b52a4de
Yoonna Lee <71828438+yoonnable@users.noreply.github.com>
2023-05-15 16:55:43 +0900

    Section02 problem03

---
+++ src/study/intro/section02/Section0203.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+  
+public class Main {
+  public String solution(int n, int[] a, int[] b) {
+    String answer = "";
+  	for(int i = 0; i < n; i++) {
+    	if(a[i] == b[i]) answer += "D";
+      	else if(a[i] == 1 && b[i] == 3) answer += "A";
+      	else if(a[i] == 2 && b[i] == 1) answer += "A";
+      	else if(a[i] == 3 && b[i] == 2) answer += "A";
+      	else answer += "B";
+    }
+    return answer;
+  }
+  
+  public static void main(String[] args){
+    Main m = new Main();
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    int[] a = new int[n];
+    int[] b = new int[n];
+    for(int i = 0; i < n; i++) {
+    	a[i] = in.nextInt();
+    }
+    for(int i = 0; i < n; i++) {
+    	b[i] = in.nextInt();
+    }
+    
+    for(char c : m.solution(n, a, b).toCharArray()) System.out.println(c);
+    return ;
+  }
+}
