b0f833129142bef9cb9dc685ac06beeb59b47868
Yoonna Lee <71828438+yoonnable@users.noreply.github.com>
2023-07-26 17:12:44 +0900

    섹션2 문제7~12 연습 2회차

--- src/study/intro/section02/practice/S020112P02.java
+++ src/study/intro/section02/practice/S020112P02.java
@@ -9,55 +9,178 @@ public class S020112P02 {
         // problem03(); //가위 바위 보  
         // problem04(); //피보나치 수열
         // problem05(); //소수(에라토스테네스 체)
-        problem06(); //뒤집은 소수
+        // problem06(); //뒤집은 소수
+        // problem07(); //점수 계산
+        // problem08(); // 등수 구하기
+        // problem09(); //격자판 최대합
+        // problem10(); //봉우리
+        // problem11(); //임시 반장 구하기
+        problem12(); //멘토링
     }
-    public void solution12() {
-
+    public int solution12(int n, int m, int[][] arr) {
+        int answer = 0;
+        for(int to = 1; to < n + 1; to++) {
+            for(int ti = 1; ti < n + 1; ti++) {
+                boolean check = true;
+                for(int i = 0; i < m; i++) {
+                    int tog = 0, tig = 0;
+                    for(int j = 0; j < n; j++) {
+                        if(arr[i][j] == to) tog = j;
+                        if(arr[i][j] == ti) tig = j;
+                    }
+                    if(tog >= tig) {
+                        check = false;
+                        break;
+                    }
+                }
+                if(check) answer++;
+            }
+        }
+        return answer;
     }
     public static void problem12() {
         S020112P02 s2 = new S020112P02();
         Scanner kb = new Scanner(System.in);
+        //반 학생 수 : n
+        //시험 치루는 횟수 : m
+        //n = 4일 때 배열 한 줄에 2 4 1 3 이면, 1등이 2번 학생, 2등이 4번학생, 3등이 1번학생 4등이 3번학생
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[][] arr = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(s2.solution12(n, m, arr));
     }
 
-    public void solution11() {
-
+    public int solution11(int n, int[][] arr) {
+        int answer = 0, max = 0, sum = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < 5; k++) {
+                    if(arr[i][k] == arr[j][k]) {
+                        sum++;
+                        break;
+                    }
+                }
+            }
+            if(max < sum) {
+                answer = i + 1;
+                max = sum;
+            }
+            sum = 0;
+        }
+        return answer;
     }
     public static void problem11() {
         S020112P02 s2 = new S020112P02();
         Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n][5];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 5; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.print(s2.solution11(n, arr));
     }
 
-    public void solution10() {
-
+    public int solution10(int n, int[][] arr) {
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                //상 arr[i - 1][j], i == 0 인 건 패스
+                //하 arr[i + 1][j], i == n - 1 인 건 패스
+                //좌 arr[i][j - 1], j == 0 인 건 패스
+                //우 arr[i][j + 1], j == n - 1 인 건 패스
+                if((i == 0 || arr[i - 1][j] < arr[i][j]) && 
+                (i == n - 1 || arr[i + 1][j] < arr[i][j]) && 
+                (j == 0 || arr[i][j - 1] < arr[i][j]) && 
+                (j == n - 1 || arr[i][j + 1] < arr[i][j])
+                ) answer++;
+            }
+        }
+        return answer;
     }
     public static void problem10() {
         S020112P02 s2 = new S020112P02();
         Scanner kb = new Scanner(System.in);
-
+        int n = kb.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(s2.solution10(n, arr));
     }
 
-    public void solution09() {
-
+    public int solution09(int n, int[][] arr) {
+        int answer = 0, crossLeftSum = 0, crossRightSum = 0;
+        for(int i = 0; i < n; i++){
+            int lowSum = 0, colSum = 0;
+            for(int j = 0; j < n; j++) {
+                lowSum += arr[i][j];
+                colSum += arr[j][i];
+            }
+            if(lowSum > answer) answer = lowSum;
+            if(colSum > answer) answer = colSum;
+            crossLeftSum += arr[i][i];
+            crossRightSum += arr[i][n - 1 - i];
+        }
+        if(crossLeftSum > answer) answer = crossLeftSum;
+        if(crossRightSum > answer) answer = crossRightSum;
+        return answer;
     }
     public static void problem09() {
         S020112P02 s2 = new S020112P02();
         Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(s2.solution09(n, arr));
     }
 
-    public void solution08() {
-
+    public int[] solution08(int n, int[] arr) {
+        int[] answer = new int[n];
+        for(int i = 0; i < n; i++) {
+            answer[i] = 1;
+            for(int j = 0; j < n; j++) {
+                if(arr[i] < arr[j]) answer[i]++;
+            }
+        }
+        return answer;
     }
     public static void problem08() {
         S020112P02 s2 = new S020112P02();
         Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        for(int i : s2.solution08(n, arr)) System.out.print(i + " ");
     }
 
-    public void solution07() {
-
+    public int solution07(int n, int[] arr) {
+        int answer = 0, score = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 1) answer += ++score;
+            else score = 0;
+        }
+        return answer;
     }
     public static void problem07() {
         S020112P02 s2 = new S020112P02();
         Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        System.out.println(s2.solution07(n, arr));
     }
 
     public boolean helper(int num) {
